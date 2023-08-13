5b1365ea8a5294f6728f2e1943f550072cc57951
Munya <munirashovokhobova@gmail.com>
2023-08-14 00:24:11 +0500

    adding gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 const path = require('path')
 module.exports = defineConfig({
+  publicPath : '/chats/',
   transpileDependencies: true,
   configureWebpack:{
     resolve:{
